3545b26f307241086ccc933f5a775ec2f28fed87
shuguang@tongbanjie.com <523686945@qq.com>
2018-03-29 21:25:05 +0800

    x

---
+++ src/test/java/com/key/jorigin/test/Test.java
@@ -0,0 +1,16 @@
+package com.key.jorigin.test;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by shuguang on 18/3/23.
+ */
+public class Test {
+
+    public void test(){
+        ConcurrentHashMap map = new ConcurrentHashMap();
+        map.put("", "");
+        map.get("");
+    }
+
+}
