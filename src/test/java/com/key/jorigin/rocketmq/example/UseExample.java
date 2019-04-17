47c74f8ce6db085cbe4a5ef5fdbcd97c698563af
zwh <zhangwenhui@kongapi.com>
2019-04-17 21:05:35 +0800

    x

--- src/test/java/com/key/jorigin/rocketmq/example/UseExample.java
+++ src/test/java/com/key/jorigin/rocketmq/example/UseExample.java
@@ -23,6 +23,7 @@ public class UseExample {
     private void producer() {
 
         /**
+         * todo :rocketmq开发指南
          * Producer Group这个概念发送普通的消息时，作用不大，但是发送分布式事务消息时，比较关键，
          * 因为服务器会回查这个Group下的任意一个Producer
          */
@@ -86,9 +87,24 @@ public class UseExample {
          */
         message.setTags("tag_test");
 
+
+        /**
+         * todo: rocketmq开发指南
+         *  每个消息在业务局面的唯一标识码，要设置到 keys 字段，方便将来定位消息丢失问题。服务器会为每个消
+         *  息创建索引(哈希索引)，应用可以通过 topic，key 来查询返条消息内容，以及消息被谁消费。由亍是哈希 索引，请务必保证 key 尽可能唯一，返样可以避免潜在的哈希冲突
+         */
+        message.setKeys("orderIdxxxxx");
+
         rocketMqProducer.sendMessage(message);
 
+
         //某些应用如果不关注消息是否发送成功，请直接使用sendOneWay方法发送消息
+        //todo:rocketmq开发指南
+        // 一个 RPC 调用，通常是返样一个过程
+        //1. 客户端収送请求到服务器
+        //2. 服务器处理该请求
+        //3. 服务器吐客户端迒回应答
+        //所以一个 RPC 的耗时时间是上述三个步骤的总和，而某些场景要求耗时非常短，但是对可靠性要求幵丌高，例如 日志收集类应用，此类应用可以采用 oneway 形式调用，oneway 形式只収送请求丌等待应答，而収送请求在客 户端实现局面仁仁是一个 os 系统调用的开销，即将数据写入客户端的 socket 缓冲区，此过程耗时通常在微秒级。
     }
 
     private void sendBySort() {
@@ -105,7 +121,7 @@ public class UseExample {
          *
          * 4. 使用了keys, 消费者也就无法进行平均分配，负载均衡
          *
-         * 5. todo 上面总结的有点小问题 设置了keys应该消息还是会存储在consume_queue中，只是指定了发给某一个queue；消费端拉取消息时，
+         * 5. todo 上面总结的有点小问题 设置了keys应该消息还是会存储在consume_queue中，只是指定了发给某一个queue（如果根据keys做select,如下代码）；消费端拉取消息时，
          *  todo 被分配到该queue的消费者单独对它消费; 这样消费者才能区分topic进行消费；
          *
          */
